08cc8514b1a10a05debc43fe1c41fc6aaa127f26
Umwizer <ruthumwizerwa@gmail.com>
2025-12-15 10:14:09 +0200

    revision of exam

---
+++ revisionofexam/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("🎉 GCC is working in VS Code!\n");
+    printf("Compiler: MinGW-W64 13.1.0\n");
+    printf("Ready for SEMESTER 3 C programming!\n");
+    
+    // Test a calculation
+    int a = 10, b = 20;
+    printf("Sum: %d + %d = %d\n", a, b, a + b);
+    
+    return 0;
+}
